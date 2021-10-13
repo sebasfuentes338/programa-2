bb1bdc025be5499b520dfa41504f0e30c89dff10
Sebastianfuent6 <90064482+sebasfuentes338@users.noreply.github.com>
2021-10-13 15:00:37 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+int lustro=5;
+int segundos=0;
+int casa=0;
+segundos=lustro*365*24*60*60;
+cout<<"en un lustro hay"<<segundos<<"segundos";
+casa=casa+1;
+
+}
